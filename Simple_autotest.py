30f8624c9ad41549e68ea64896bf0b2adc1910d1
YuraVasilev88 <goodduo73@gmail.com>
2024-12-01 10:58:21 +0300

    Test from Lesson_12

--- Simple_autotest.py
+++ Simple_autotest.py
@@ -4,4 +4,4 @@ assert hello_who('Max') == 'Hello,Max', 'Hello who error'
 assert hello_who('Leo') == 'Hello,Leo', 'Hello who error'
 assert hello_who('Nikita') == 'Hello,Nikita', 'Hello who error'
 assert salary(2,1) == 4
-assert salary(2,2) == 8
+assert salary(3,3) == 9
